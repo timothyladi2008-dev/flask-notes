c18cc14396a0ff855ee2ed89c0ab75348bbcc416
timothyladi2008-dev <timothyladi2008@gmail.com>
2026-08-10 23:44:19 +0100

    Add files via upload

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,6 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
@@ -9,20 +9,28 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshdjs'
+
+    # Secret key for securing sessions & cookies
+    app.config['SECRET_KEY'] = 'dev-secret-key-change-in-production'
+
+    # Database path setup
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
 
+    # Import blueprints
     from .views import views
     from .auth import auth
 
+    # Register blueprints
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    # Import models so SQLAlchemy registers them
     from .models import User, Note
 
-    create_database(app)
-
+    # Setup Flask-Login session management
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
@@ -31,11 +39,8 @@ def create_app():
     def load_user(id):
         return User.query.get(int(id))
 
+    # Create database tables within application context
+    with app.app_context():
+        db.create_all()
+
     return app
-
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        with app.app_context():
-            db.create_all()
-        print('Created Database!')
