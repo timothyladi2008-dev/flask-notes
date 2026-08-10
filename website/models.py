8146fa78d43ea2616aa71d6430cf7df75db70595
timothyladi2008-dev <timothyladi2008@gmail.com>
2026-08-10 23:34:17 +0100

    Add files via upload

--- website/models.py
+++ website/models.py
@@ -2,12 +2,18 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
-    # Automatically sets current date and time
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    # New note features
+    color = db.Column(db.String(20), default='#ffffff')  # Card background color
+    is_pinned = db.Column(db.Boolean, default=False)  # Pinned status
+    is_deleted = db.Column(db.Boolean, default=False)  # Soft delete (Trash bin)
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,3 +21,6 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+
+    # User preferences
+    default_note_color = db.Column(db.String(20), default='#ffffff')
